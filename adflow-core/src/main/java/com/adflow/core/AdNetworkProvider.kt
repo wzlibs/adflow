d9d167860e982e4e65c9e87ef6456daa0c433d4f
Pham Van Nam <pvnam198@gmail.com>
2026-07-05 21:31:38 +0700

    Add AdNetworkProvider as the single ad-network swap point

---
+++ adflow-core/src/main/java/com/adflow/core/AdNetworkProvider.kt
@@ -0,0 +1,12 @@
+package com.adflow.core
+
+import android.content.Context
+
+interface AdNetworkProvider {
+    fun initialize(context: Context, onComplete: () -> Unit = {})
+    fun createInterstitial(config: PlacementConfig): InterstitialAdManager
+    fun createAppOpen(config: PlacementConfig): AppOpenAdManager
+    fun createRewarded(config: PlacementConfig): RewardedAdManager
+    fun createNative(config: PlacementConfig): NativeAdManager
+    fun createBanner(config: PlacementConfig): BannerAdManager
+}
